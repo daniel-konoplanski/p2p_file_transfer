d0c26f4b47a5e692cc6ce150af432a1569c6b584
daniel-konoplanski <85637752+daniel-konoplanski@users.noreply.github.com>
2025-12-11 20:17:36 +0100

    Added missing namespace and virtual destructor to IMessageSender

--- src/lib.comms/i_sender.hpp
+++ src/lib.comms/i_sender.hpp
@@ -1,14 +1,23 @@
 #pragma once
 
 #include <functional>
+#include <memory>
 #include <system_error>
 
 #include <google/protobuf/message.h>
+
+namespace p2pft::comms
+{
 
 using SenderCallback = std::function<void(const std::error_code&, size_t)>;
 
 class IMessageSender
 {
 public:
+    virtual ~IMessageSender() = default;
+
+public:
     virtual void send(const google::protobuf::Message& message, SenderCallback callback) = 0;
 };
+
+}  // namespace p2pft::comms
